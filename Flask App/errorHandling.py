b29203c2a22b5a53d6fb9dd47938bb327f9f6fe2
Metrowe <emmetrowe@gmail.com>
2019-02-04 16:35:25 +0000

    Updated prototype for use with AWS and/or python anywhere

--- Flask App/errorHandling.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-# Throws an error with specified message and exits the program
-def manualThrow(errorMessage):
-	formattedErrorMessage = "ERROR: " + errorMessage
-	sys.exit(formattedErrorMessage)
